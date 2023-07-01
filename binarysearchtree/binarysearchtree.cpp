58ecf1cf577d52515615c90707d2fc8419cba038
andiharun29 <andiharunalrasyid14@gmail.com>
2023-07-01 13:24:35 +0700

    membuat method

--- binarysearchtree/binarysearchtree.cpp
+++ binarysearchtree/binarysearchtree.cpp
@@ -57,4 +57,17 @@ public:
 		// this function searches the currentNode of the specified Node as well as the current node of its parent
 		currentNode = ROOT;
 		parent = NULL;
+
+		currentNode = ROOT;
+		parent = NULL;
+		while ((currentNode != NULL) && (currentNode->info != element))
+		{
+			parent = currentNode;
+			if (element < currentNode->info)
+				currentNode = currentNode->leftchild;
+			else
+				currentNode = currentNode->rightchild;
+		}
+	}
+
 }
